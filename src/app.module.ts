b79dec4c7624238a1a55650e03e24e0f30e715c5
Matilda Mared <matildamared@live.se>
2022-11-28 14:18:10 +0100

    add basic resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { LessonModule } from "./lesson/lesson.module";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 
 @Module({
-    imports: [GraphQLModule.forRoot({ autoSchemaFile: true }), LessonModule],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: true,
+        }),
+        LessonModule,
+    ],
     controllers: [],
     providers: [],
 })
